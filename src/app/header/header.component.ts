ba0c1b3c2c2fc0c4d96fef602181b05f8c5a9b38
garcon franck <franck.garcon@gmail.com>
2021-09-06 14:21:37 +0200

    Header Component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,48 @@
+import { UsersService } from './../services/users.service';
+import { Subscription } from 'rxjs';
+import { Category } from './../model/category';
+import { CategoryService } from './../services/category.service';
+import { Cart } from '../model/cart';
+import { Products } from '../model/products';
+import { CartService } from '../services/cart.service';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { EnvService } from '../env.service';
+
+@Component({
+   selector: 'app-header',
+   templateUrl: './header.component.html',
+   styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+   cart: Cart[] = [];
+   cartData;
+   categories: Category[];
+   categorySub: Subscription;
+   isAuth = false;
+
+   constructor(
+      private cartService: CartService,
+      private categoryService: CategoryService,
+      private userService: UsersService,
+      private env: EnvService
+   ) {}
+
+   ngOnInit(): void {
+      this.cart = this.cartService.cart;
+      this.cartData = this.cartService.cartData;
+      this.isAuth = this.userService.isAuth;
+
+      this.categorySub = this.categoryService.categorySubject.subscribe(
+         (data: Category[]) => {
+            this.categories = data;
+         }
+      );
+      this.categoryService.emitCategories();
+   }
+
+   logout() {
+      this.userService.logout();
+      this.isAuth = this.userService.isAuth;
+   }
+}
