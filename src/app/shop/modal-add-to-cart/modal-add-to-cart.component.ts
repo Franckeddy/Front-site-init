9387f862a015dd84e4a76da9e94005fb4c02d71f
garcon franck <franck.garcon@gmail.com>
2021-09-14 10:18:55 +0200

    modal translations

--- src/app/shop/modal-add-to-cart/modal-add-to-cart.component.ts
+++ src/app/shop/modal-add-to-cart/modal-add-to-cart.component.ts
@@ -1,8 +1,8 @@
-import { environment } from './../../../environments/environment';
 import { Products } from './../../model/products';
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { EnvService } from 'src/app/env.service';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
    selector: 'app-modal-add-to-cart',
@@ -10,11 +10,16 @@ import { EnvService } from 'src/app/env.service';
    styleUrls: ['./modal-add-to-cart.component.scss'],
 })
 export class ModalAddToCartComponent implements OnInit {
-   @Input() products: Products[];
+   @Input()
+   products: Products[];
+   cartData;
 
    constructor(
       private env: EnvService,
+      private cartService: CartService,
    ) {}
 
-   ngOnInit(): void {}
+   ngOnInit(): void {
+      this.cartData = this.cartService.cartData;
+   }
 }
